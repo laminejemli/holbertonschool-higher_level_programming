9ed7ff43326ee31bf125a700d19e6c7f6bcdb09c
lamine <mokhtarjemli2@gmail.com>
2021-04-27 13:39:22 -0700

    fixed 9 was 8

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for l in str:
-        if ord(l) >= ord('a') and ord(l) <= ord('z'):
-            char = chr(ord(l) - 32)
-        else:
-            char = l
-        print("{:s}".format(char), end="")
-    print('')
+def print_last_digit(number):
+    if number < 0:
+        number = -number
+    l = number % 10
+    print(l, end='')
+    return l
