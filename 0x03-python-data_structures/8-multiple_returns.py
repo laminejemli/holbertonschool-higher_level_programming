c8cfe0df4d793721c23f79058e0e601d74fc34f2
lamine <mokhtarjemli2@gmail.com>
2021-05-04 07:14:41 -0700

    lol

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    mytup = ()
+    if len(sentence) == 0:
+        mytup = 0, "None"
+    else:
+        mytup = len(sentence), sentence[0]
+    return mytup
