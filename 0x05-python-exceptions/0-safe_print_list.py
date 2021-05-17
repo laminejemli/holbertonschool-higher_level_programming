b7a572698392339b735d0082bfa4443e65962e2b
lamine <mokhtarjemli2@gmail.com>
2021-05-17 03:27:00 -0700

    olo

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    sum = 0
+    C = 0
     for i in range(0, x):
         try:
-            print("{:d}".format(my_list[i]))
-            sum += 1
-            except:
-                print()
-                return sum
+            print("{:d}".format(my_list[i]), end="")
+            C += 1
+        except:
+            pass
+    print()
+    return C
