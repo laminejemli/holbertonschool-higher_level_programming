343a2372df78c0c473469f579fc40cfb0b92e32e
lamine <mokhtarjemli2@gmail.com>
2021-05-19 02:10:42 -0700

    lol

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 class Square:
+    """ Represents a square.
+    Private instance attribute: size:
+    Instantiation with optional size.
+    Public instance method: def area(self).
+    """
+
     def __init__(self, size=0):
+        """ Initializes size """
         self.__size = size
 
     @property
     def size(self):
+        """ Retrieves the data. """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ Sets the size. """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -16,4 +25,5 @@ class Square:
         self.__size = value
 
     def area(self):
+        """ Returns the square area. """
         return self.__size ** 2
