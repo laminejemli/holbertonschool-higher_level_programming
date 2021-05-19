fac4cbc7842ec12c51cee3029815fdda81c45676
lamine <mokhtarjemli2@gmail.com>
2021-05-19 01:21:29 -0700

    comment

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 class Square:
+    """ module represent a square class 
+    new attribute: size """
+
     def __init__(self, size):
+        """ Initializes the size paramater. """
         self.__size = size
