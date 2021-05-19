d675822ced453514af6c5abfbd727c52fda7786a
lamine <mokhtarjemli2@gmail.com>
2021-05-19 08:01:04 -0700

    lol

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-class Square:
-    """ into the module class """
+""" module is the beginning of classes """
 
-    def __init__(self, size):
-        """ set the data."""
-        if not isinstance(size, int):
+
+class Square():
+    """ class Square that defines a square """
+    def __init__(self, size=0):
+        if type(size) != int:
             raise TypeError("size an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size >= 0")
         self.__size = size
 
-    def area(self):
+        def area(self):
+            """ end and return """
         return self.__size ** 2
