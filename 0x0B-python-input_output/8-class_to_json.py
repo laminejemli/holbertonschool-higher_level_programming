a7fee64ba5a7ac69205f44e553ff6a5aa7bd93eb
lamine <mokhtarjemli2@gmail.com>
2021-06-03 15:49:17 -0700

    lol

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module 10-class_to_json.
+Returns the description with
+simple data structure for JSON serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """Creates a dict description of obj.
+    Args:
+        - obj: object to serialize
+    Returns: dictionnary description of obj
+    """
+
+    return obj.__dict__
