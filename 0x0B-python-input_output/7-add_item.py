aa4d3a36d8b52d8f60a3a18cbec9791acfe51112
lamine <mokhtarjemli2@gmail.com>
2021-06-03 15:51:32 -0700

    lkjl

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,12 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """Module 9-add_item.
 Adds all arguments to a Python list,
 and then save them to a file.
 """
 
-import os.path
-import json
 import sys
-
+import json
+import os.path
 
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
