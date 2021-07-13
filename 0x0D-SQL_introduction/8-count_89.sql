beb49ef5b7438b93952279c591b5d627778901e9
lamine <youssejemli@gmail.com>
2021-07-13 15:02:33 +0100

    comment

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
--- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0
--- displays number of records with a condition
-SELECT COUNT(*) FROM first_table WHERE if = 89;
+-- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server
+-- displays number of records fitting a condition
+SELECT COUNT(*) FROM first_table WHERE id = 89;
