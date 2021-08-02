5c674c5f3fc7c84b9abbc6cd93c6d0d117851bd7
lamine <youssejemli@gmail.com>
2021-08-02 19:12:41 +0100

    javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (ProcessingInstruction.argv.length ===2) {
+    console.log('No Argument');
+} else if (ProcessingInstruction.argv.length === 3) {
+    console.log('Argument found')
+} else {
+    console.log('Argument found');
+}
